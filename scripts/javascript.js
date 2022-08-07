cdd3c434c0506ad0e36e77d8971880b3d75f3089
YoderBy <yosefbyd@gmail.com>
2022-08-07 20:29:30 +0300

    css fixes (still look like garabge, for now...)

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -5,7 +5,9 @@ window.addEventListener('load', function () {
       add_task();
     }
   });
-
+  document.getElementById("input_task").addEventListener("click", function(stroke) {
+    document.getElementById("input_task").value ='';
+  });
 })
 function add_task() {
     const task_num = document.getElementById("answers").childElementCount; //the task identifier
@@ -13,13 +15,12 @@ function add_task() {
     const input_value = document.getElementById("input_task").value; // the content
     new_task.innerHTML = `
     
-  <button type = "button" class = "unchk" id = "task${task_num}_btn" onclick = "move_to_done(${task_num})"></button>
+  <button type = "button" class = "unchk" id = "task${task_num}_btn" onclick = "move_to_done(${task_num})">Done!</button>
   
   <input type = "text" id = "val_task${task_num}" value = "${input_value}""> 
   
   <button type = "button" class = "rmv" onclick = "remove_task(${task_num})">remove</button>
   
-  <p>  ${task_num} </p>
   `;
   
   new_task.id = `task_${task_num}`;
@@ -46,7 +47,7 @@ function move_to_done(num) {
     re_born.setAttribute("id",`task_${num}`);
     re_born.children[1].classList.add("strike_through");
     re_born.children[0].setAttribute("onclick", `revive(${num})`); //changign the button action
-    
+    re_born.children[0].innerHTML = "ToDo"
     victim_div.remove();
   }
   
@@ -67,7 +68,8 @@ function revive(num) {
     
     re_born.setAttribute("id",`task_${num}`) 
     re_born.children[0].setAttribute("onclick", `move_to_done(${num})`); //changign the button action
-    
+    re_born.children[0].innerHTML = "Done!"
+
     victim_div.remove();
   }
 
