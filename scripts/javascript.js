b370c761984fa3a388ce7759e44bc5aac1f89ddf
YoderBy <yosefbyd@gmail.com>
2022-09-14 22:47:07 +0300

    fixed xss breach in newTask

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -2,43 +2,46 @@ window.addEventListener('load', function () {
 
   document.getElementById("input_task").addEventListener("keypress", function(stroke) { //CR Minor - could be an anonymous function
     if (stroke.key == "Enter") {
-      add_task();
+      const task_num = document.getElementById("answers").childElementCount;
+      const input_value = document.getElementById("input_task").value;
+      addTask(task_num , input_value);
     }
   });
   document.getElementById("input_task").addEventListener("click", function(stroke) { //CR Minor - seperate getElementById from actual code logic. save the element as a const
     document.getElementById("input_task").value ='';
   });
 })
-function add_task() { 
+function addTask(task_num, input_value) { 
   //CR Major - this function should get the data it needs as parameters, not look for it itself
   //CR Minor - naming conventions. function names should be in camelCase, not snake_case 
-    const task_num = document.getElementById("answers").childElementCount; //the task identifier 
-    const new_task = document.createElement('div');
-    const input_value = document.getElementById("input_task").value; // the content
-    new_task.innerHTML = `
+  const newTask = document.createElement('div');
+  const pure_task_content = purify(input_value);
+  const pure_task_num = purify(task_num);
+    newTask.innerHTML = `
     
-  <button type = "button" class = "unchk" id = "task${task_num}_btn" onclick = "move_to_done(${task_num})">Done!</button>
+  <button type = "button" class = "unchk" id = "task${task_num}_btn" onclick = "moveToDone(${task_num})">Done!</button>
   
-  <textarea style = "width: 50%" id = "val_task${task_num}">${input_value}</textarea> 
+  <textarea style = "width: 50%" id = "val_task${task_num}">${pure_task_content}</textarea> 
   
-  <button type = "button" class = "rmv" onclick = "remove_task(${task_num})">remove</button>
+  <button type = "button" class = "rmv" onclick = "removeTask(${pure_task_num})">remove</button>
   
   `; //CR Minor - if you use CSS don't also use the style property
   //CR Major - don't add HTML directly as a string, this is just asking for XSS to happen
-  new_task.id = `task_${task_num}`;
-  new_task.classList.add("task");
+
+  newTask.id = `task_${pure_task_num}`;
+  newTask.classList.add("task");
   document.getElementById("input_task").value = '';
-  document.getElementById("answers").appendChild(new_task);
+  document.getElementById("answers").appendChild(newTask);
   }
   //CR Major - each function should be responsible to do one thing, this function fetches the data from the input, generates a new Task element while caring about all the other tasks, adds the task to the DOM, and empties the input
 
-function remove_task(num) {
+function removeTask(num) {
   //CR Minor - if you are using num as an identifier, might want to call it ID
     const victim_div = document.getElementById(`task_${num}`);
     victim_div.remove();
   }
   
-function move_to_done(num) {
+function moveToDone(num) {
     
     const victim_div = document.getElementById(`task_${num}`);
     const re_born = document.createElement('div');
@@ -57,7 +60,7 @@ function move_to_done(num) {
   
 function revive(num) {
   //CR Minor - this is a fun name but not a very indicative one. this implies the function return a deleted task, not a done task
-    //it should mimic move_to_done, but in reverse. It's so similar, I wonder if I should  just add a "location" parameter and merge them
+    //it should mimic moveToDone, but in reverse. It's so similar, I wonder if I should  just add a "location" parameter and merge them
     //CR - this is indeed code duplication
 
     const victim_div = document.getElementById(`task_${num}`);
@@ -73,13 +76,13 @@ function revive(num) {
     re_born.children[1].classList.remove('strike_through');
     
     re_born.setAttribute("id",`task_${num}`) 
-    re_born.children[0].setAttribute("onclick", `move_to_done(${num})`); //changign the button action
+    re_born.children[0].setAttribute("onclick", `moveToDone(${num})`); //changign the button action
     re_born.children[0].innerHTML = "Done!"
 
     victim_div.remove();
   }
 
-function hide_done(){
+function hideDone(){
   flag = document.getElementById("flag").innerHTML;
   //CR Minor - will you have no other flags? maybe the ID should be more unique
   if (flag == 0){
@@ -96,7 +99,7 @@ function hide_done(){
   }
 }
 
-function hide_done2(){
+function hideDone2(){
   isVisible = !!document.getElementById("flag").innerHTML;
   document.getElementById("done_tasks_tab").style.display = isVisible ? "flex" : "none";
   document.getElementById("hide/show_btn").innerHTML = isVisible ? "Hide" : "Show";
@@ -104,3 +107,8 @@ function hide_done2(){
 }
 //CR Minor - without changing the functionality at all (which I think you should), look how much more elegant this function could be
 
+function purify(str){//from the web https://portswigger.net/web-security/cross-site-scripting/preventing
+  return String(str).replace(/[^\w. ]/gi, function(c){
+      return '&#'+c.charCodeAt(0)+';';
+  });
+}
