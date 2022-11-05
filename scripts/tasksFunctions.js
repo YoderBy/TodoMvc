890b1c006e980ff4005251e8b967d32af5237581
YoderBy <yosefbyd@gmail.com>
2022-11-05 17:01:04 +0200

    fixed generate task string HTML issue

--- scripts/tasksFunctions.js
+++ scripts/tasksFunctions.js
@@ -1,34 +1,87 @@
-function generateTask(task_num, title_input_value, body_input_value) {
+function generateTask(taskNum, titleInputValue, bodyInputValue) {
+
   const cur_time = getTime();
   const new_task = document.createElement('div');
-  const pure_title_task_content = purify(title_input_value);
-  const pure_body_task_content = purify(body_input_value);
-  const pure_task_num = purify(task_num);
-  new_task.innerHTML = `
-    <div class = "visible_task">
-      <div id = "${pure_task_num}_is_editable" style = "display:none">1
-      </div>
-      <button type = "button" class = "unchk" id = "task${pure_task_num}_btn" onclick = "moveTo(${pure_task_num},document.getElementById('done_tasks_tab'))">
-      </button>
-      <div class = "task_input">
-        <input class = "task_title" disabled="disabled" id = "title_val_task${pure_task_num}" value = '${pure_title_task_content}'/> 
-        <input class = "task_value" disabled="disabled" id = "text_val_task${pure_task_num}" value = '${pure_body_task_content}'/> 
-      </div>
-      <button type = "button" class = "edt yosef-mdc-button-outlined" id = "edit_btn${pure_task_num}" onclick = "editTask(${pure_task_num})">Edit
-      </button>
-      <div class = "time_box">
-        <label class = "task_time_description" id = "task_time_description${pure_task_num}">Added at:
-        </label>
-        <label class = "task_time" id = "task_time${pure_task_num}">${cur_time}
-        </label>
-      </div>
-      <button type = "button" class = "rmv" onclick = "removeTask(${pure_task_num})"></button>
-    </div>
-    `;
+  const pure_title_task_content = purify(titleInputValue);
+  const pure_body_task_content = purify(bodyInputValue);
+  const pure_task_num = purify(taskNum);
+
+  const visible_task = document.createElement('div');
+  visible_task.classList.add("visible_task");
+  new_task.appendChild(visible_task);
+  
+  const div_task_iseditable = document.createElement('div');
+  div_task_iseditable.setAttribute('id', `${pure_task_num}_is_editable`);
+  div_task_iseditable.style.display = 'none';
+  div_task_iseditable.innerHTML = 1;
+  visible_task.appendChild(div_task_iseditable);
+
+  const uncheck_button = document.createElement('button');
+  uncheck_button.classList.add("unchk");
+  uncheck_button.setAttribute('id',`task${pure_task_num}_btn`);
+  uncheck_button.setAttribute('onclick',`moveTo(${pure_task_num},document.getElementById('done_tasks_tab'))`);
+  visible_task.appendChild(uncheck_button);
+
+  const task_input = document.createElement('div');
+  task_input.classList.add('task_input');
+
+  visible_task.appendChild(task_input);
+
+  const task_title = document.createElement('input');
+  task_title.classList.add('task_title');
+  task_title.setAttribute('disabled','disabled');
+  task_title.setAttribute('id', `title_val_task${pure_task_num}`);
+  task_title.setAttribute('value', pure_title_task_content);
+
+  const task_value = document.createElement('input');
+  task_value.classList.add('task_value');
+  task_value.setAttribute('disabled','disabled');
+  task_value.setAttribute('id', `text_val_task${pure_task_num}`);
+  task_value.setAttribute('value', pure_body_task_content);
+  
+  task_input.appendChild(task_title);
+  task_input.appendChild(task_value);
+
+  
+  const edit_button = document.createElement('button');
+  edit_button.classList.add("edt");
+  edit_button.classList.add("yosef-mdc-button-outlined");
+  edit_button.setAttribute('id',`edit_btn${pure_task_num}`);
+  edit_button.setAttribute('onclick',`editTask(${pure_task_num})`);
+  edit_button.innerHTML = "EDIT";
+  visible_task.appendChild(edit_button);
+
+  const time_box = document.createElement('div');
+  time_box.classList.add("time_box");
+  
+  const time_box_des = document.createElement('label');
+  time_box_des.classList.add('task_time_description');
+  time_box_des.setAttribute('id', `task_time_description${pure_task_num}`)
+  time_box_des.innerHTML = 'Added at:';
+
+  const time_box_time = document.createElement('label');
+  time_box_time.classList.add('task_time');
+  time_box_time.setAttribute('id', `task_time${pure_task_num}`)
+  time_box_time.innerHTML = cur_time;
+
+  time_box.appendChild(time_box_des);
+  time_box.appendChild(time_box_time);
+
+  visible_task.appendChild(time_box);
+
+  const remove_button = document.createElement('button');
+  remove_button.classList.add("rmv");
+  remove_button.setAttribute('onclick',`removeTask(${pure_task_num})`);
+  visible_task.appendChild(remove_button);
+
+
   new_task.id = `task_${pure_task_num}`;
   new_task.classList.add("task");
   //CR Major - this works as a way to avoid XSS, but it's better to avoid writing HTML as strings. use document.createElement
+  //I hope your'e happy now. couse I'm not
   return new_task;
+
+
 }
 
 function addTask(titleInputValue, bodyInputValue, taskLocation) {
