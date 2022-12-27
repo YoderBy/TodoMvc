752d2a5601750388d728fb280e33dcb080406a73
Yosef Ben Yehuda <48494974+YoderBy@users.noreply.github.com>
2022-12-27 16:59:06 +0200

    change hide done parmaeter name

--- scripts/tasksFunctions.js
+++ scripts/tasksFunctions.js
@@ -6,21 +6,21 @@ window.localStorage.setItem("is_hidden", "0");
 function moveTo(id, location) {
   const victim_div = document.getElementById(`task_${id}`);
   const re_born = victim_div.cloneNode(true);//https://stackoverflow.com/questions/19482076/how-to-duplicate-a-div-in-javascript
-  location.appendChild(re_born);
+  location.appendChild(re_born); 
   victim_div.remove();
-  //CR Minor - no need to delete and recreate, you can just move the node
+  //CR Minor - no need to delete and recreate, you can just move the node ((searched for methid for this. not found))
   modifyTask(id);
 }
 
 function hideDone() {
-  isVisible = !!parseInt(window.localStorage.getItem("isHidden"))
-  //CR Minor - good use of localStorage, but seeing as this is global for the entire app this should have a more indicative name
-  document.getElementById("done_tasks_tab").style.display = isVisible ? "block" : "none";
-  document.getElementById("hide/show_btn").innerHTML = isVisible ? "Hide" : "Show";
-  isHidden= isVisible ? "0" : "1";
+  hide_toggle_state = !!parseInt(window.localStorage.getItem("isHidden"))
+  //CR Minor - good use of localStorage, but seeing as this is global for the entire app this should have a more indicative name ((FIXED))
+  document.getElementById("done_tasks_tab").style.display = hide_toggle_state ? "block" : "none";
+  document.getElementById("hide/show_btn").innerHTML = hide_toggle_state ? "Hide" : "Show";
+  isHidden= hide_toggle_state ? "0" : "1";
   window.localStorage.setItem("isHidden", isHidden)
   
-  if(isVisible){
+  if(hide_toggle_state){
     document.getElementById("hide/show_btn").classList.add("yosef-mdc-button-outlined");
     document.getElementById("hide/show_btn").classList.remove("yosef-mdc-button");  
   }
